e3f8776f589aeae134962a4347359efb61d26dd3
adityathodsare <thodsareaditya@gmail.com>
2025-12-28 18:30:01 +0530

    removed eslint issue

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["images.pexels.com", "images.unsplash.com"],
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
